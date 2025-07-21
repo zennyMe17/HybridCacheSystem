4143f08c1395f7b4d0abadd9e066428f3d79ed2d
Hemanth S <gowda.hemanth.1718@gmail.com>
2025-07-21 21:33:55 +0530

    Update CacheManager.cpp

--- src/CacheManager.cpp
+++ src/CacheManager.cpp
@@ -19,9 +19,15 @@ void CacheManager::accessCache(int key, int value) {
     }
 }
 
-void CacheManager::switchPolicy() {
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+// Modified to use the stop flag
+void CacheManager::switchPolicy(std::atomic<bool>& stop_flag) {
+    // Loop only until the flag is set to true
+    while (!stop_flag.load()) {
+        std::this_thread::sleep_for(std::chrono::seconds(2)); // Shortened for test
+        
+        // Check the flag again in case it was set while sleeping
+        if (stop_flag.load()) break;
+
         setPolicy(current_policy == LFU ? LRU : LFU);
         std::cout << "Cache policy switched!" << std::endl;
     }
